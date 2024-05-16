2558d7a40cab9012c20c7c87dabc19d16a2af9b1
WildBabaloo <andphiltre@gmail.com>
2024-05-16 18:07:21 -0400

    Basic bot connection with no commands

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import {Client, Events, GatewayIntentBits} from "discord.js";
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.once(Events.ClientReady, (c) => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+  });
+
+client.login(process.env.DISCORD_TOKEN);
