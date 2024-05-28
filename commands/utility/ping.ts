3f46b2c051b4bb182640fb7f8d080ade044b02c9
WildBabaloo <andphiltre@gmail.com>
2024-05-28 00:25:14 -0400

    Mini whoops

--- commands/utility/ping.ts
+++ commands/utility/ping.ts
@@ -4,7 +4,11 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
-	async execute(interaction: { reply: (arg0: string) => any; }) {
+	async execute(interaction: {
+		user: any; reply: (arg0: string) => any; 
+}) {
+		const currentTime = new Date().toISOString().replace(/T/, " ").replace(/\..+/, " ");
+		console.log(`This command was done by ${interaction.user.username} (ID: ${interaction.user.id}) at ${currentTime}`);
 		await interaction.reply('Pong!');
 	},
 };
--- index.ts
+++ index.ts
@@ -17,7 +17,7 @@ const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith('.js'));
+	const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith('.ts'));
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
