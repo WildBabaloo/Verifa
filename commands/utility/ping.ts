2ad9c439ea8ffa85e8c656a0dd9241a60f43027d
WildBabaloo <andphiltre@gmail.com>
2024-06-02 22:30:50 -0400

    Linter changes for ping.ts

--- commands/utility/ping.ts
+++ commands/utility/ping.ts
@@ -1,12 +1,10 @@
-const { SlashCommandBuilder } = require('discord.js');
+import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction: {
-		user: any; reply: (arg0: string) => any; 
-}) {
+		.setName("ping")
+		.setDescription("Replies with Pong!"),
+	async execute(interaction: CommandInteraction) {
 		const currentTime = new Date().toISOString().replace(/T/, " ").replace(/\..+/, " ");
 		console.log(`This command was done by ${interaction.user.username} (ID: ${interaction.user.id}) at ${currentTime}`);
 		await interaction.reply('Pong!');
