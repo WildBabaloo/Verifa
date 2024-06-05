566cbd658a8a883cfed16aef5901d65e08c6bcc8
WildBabaloo <andphiltre@gmail.com>
2024-06-05 00:26:22 -0400

    Basic embed added

--- commands/lockdown/setLockdown.ts
+++ commands/lockdown/setLockdown.ts
@@ -1,9 +1,30 @@
-import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
+import { SlashCommandBuilder, CommandInteraction, EmbedBuilder } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
 	.setName('setlockdown')
 	.setDescription('Updates the lockdown settings of the server');
 
 export async function execute(interaction: CommandInteraction) {
-	await interaction.reply('test');
+	// await interaction.reply('test');
+	interaction.channel?.send({ embeds: [embedBuilder()] })
+}
+
+function embedBuilder(): EmbedBuilder {
+	return new EmbedBuilder()
+	.setColor(0x0099FF)
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setTimestamp()
+	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 }
