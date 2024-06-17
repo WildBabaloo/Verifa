d2b1a93fad133d8faf3f826e78aafc6f23e540f2
WildBabaloo <andphiltre@gmail.com>
2024-06-17 13:50:25 -0400

    makeNewUserDocumentWithLockdown function finished

--- commands/lockdown/lockdown_user.ts
+++ commands/lockdown/lockdown_user.ts
@@ -1,6 +1,7 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
-import { Server } from '../../database/schemas/servers';
-import { User } from '../../database/schemas/users';
+import { SlashCommandBuilder, ChatInputCommandInteraction, User } from 'discord.js';
+import { Servers } from '../../database/schemas/servers';
+import { Users } from '../../database/schemas/users';
+import { Mongoose } from 'mongoose';
 
 export const data = new SlashCommandBuilder()
 		.setName("lockdown")
@@ -15,6 +16,34 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 		await interaction.reply({content: "Error! The user is invalid", ephemeral: true});
 		return;
 	}
-
+    const serverID = interaction.guild?.id as string;
+    const serverName = interaction.guild?.name as string;	
+	await addUserToTheDatabase(user, serverID, serverName);
 	interaction.reply(`User ${user} has been put in lockdown`);
+}
+
+async function addUserToTheDatabase(user: User, serverID: string, serverName: string) {
+	try {
+		let user = await Users.findOne({})
+	} catch (error) {
+		console.error(`Error adding to the database for user: ${user}, serverID: ${serverID} and serverName: ${serverName}`, error);
+        return null; 
+	}
+}
+
+async function makeNewUserDocumentWithLockdown(userId: string, username: string, serverID: string, serverName: string) {
+	return new Users({
+		id: userId,
+		username: username,
+		userLogs: {
+			globalBans: null,
+			activeLockdowns: {
+				server: {
+					serverID: serverID, 
+					serverName: serverName,
+				}
+			},
+			notes: null,
+		}
+	})
 }
