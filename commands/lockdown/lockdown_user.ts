6bc884d8b3b965b5328c259dfefe9e1e6ca3fb79
WildBabaloo <andphiltre@gmail.com>
2024-07-03 17:32:53 -0400

    getLockdownRoleID

--- commands/lockdown/lockdown_user.ts
+++ commands/lockdown/lockdown_user.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction, User } from 'discord.js';
-// import { Servers } from '../../database/schemas/servers';
+import { Servers } from '../../database/schemas/servers';
 import { Users } from '../../database/schemas/users';
 
 export const data = new SlashCommandBuilder()
@@ -18,12 +18,17 @@ export async function execute(interaction: ChatInputCommandInteraction) {
     const serverID = interaction.guild?.id as string;
     const serverName = interaction.guild?.name as string;	
 	await addUserToTheDatabase(user, serverID, serverName);
+	const lockdownRoleID = await getLockdownRoleID(serverID);
+	if (!lockdownRoleID) {
+		await interaction.reply({content: `Error! The <@&${lockdownRoleID}> does not exist anymore or has an unknown issue with it. Please use another role instead`});
+		return;
+	}
 	await interaction.reply(`User ${user.globalName} (ID: ${user.id}) has been put in lockdown`);
 }
 
 async function addUserToTheDatabase(user: User, serverID: string, serverName: string) {
 	try {
-		let theUser = await Users.findOne({ id: user.id })
+		let theUser = await Users.findOne({ id: user.id });
 		if (!theUser) {
 			console.log(`User ${user.globalName} (ID: ${user.id}) was not found in the database. Adding it now...`);
 			theUser = makeNewUserDocumentWithLockdown(user.id, user.globalName as string, serverID, serverName)
@@ -34,6 +39,12 @@ async function addUserToTheDatabase(user: User, serverID: string, serverName: st
 		console.error(`Error adding to the database for user: ${user.globalName} (ID: ${user.id}), serverID: ${serverID} and serverName: ${serverName}`, error);
         return null; 
 	}
+}
+
+async function getLockdownRoleID(serverID: string) {
+	const theServer = await Servers.findOne({ id: serverID });
+	if (!theServer) return null;
+	return theServer.serverConfig?.lockdownRoleID;
 }
 
 function makeNewUserDocumentWithLockdown(userId: string, username: string, serverID: string, serverName: string) {
