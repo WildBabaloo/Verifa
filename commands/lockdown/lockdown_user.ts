fb176c22312f3f85e6e0128dcec6acef06aaea47
WildBabaloo <andphiltre@gmail.com>
2024-07-13 17:29:38 -0400

    Now sends a log message after a user has been put into lockdown

--- commands/lockdown/lockdown_user.ts
+++ commands/lockdown/lockdown_user.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, User, GuildMember, EmbedBuilder } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction, User, GuildMember, EmbedBuilder, TextChannel } from 'discord.js';
 import { Servers } from '../../database/schemas/servers';
 import { Users } from '../../database/schemas/users';
 
@@ -15,10 +15,16 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 		await interaction.reply({content: "Error! The user is invalid", ephemeral: true});
 		return;
 	}
+	const currentChannel = interaction.channel;
+	if (!currentChannel) { 
+		console.error("Interaction channel is null."); 
+		await interaction.reply({content: "Error processing this command due to an unknown error (Most likely of discord's part)", ephemeral: true});
+		return;
+	}
     const serverID = interaction.guild?.id as string;
     const serverName = interaction.guild?.name as string;	
 	await addUserToTheDatabase(user, serverID, serverName);
-	const lockdownRoleID = await getLockdownRoleID(serverID);
+	const lockdownRoleID = await getLockdownRoleIDFromDatabase(serverID);
 	if (!lockdownRoleID) {
 		await interaction.reply({content: `Error! The <@&${lockdownRoleID}> does not exist anymore or has an unknown issue with it. Please use another role instead`});
 		return;
@@ -30,6 +36,17 @@ export async function execute(interaction: ChatInputCommandInteraction) {
         await member.roles.add(lockdownRoleID);
         await interaction.reply(`<@${user.id}> has been put into lockdown mode`);
 		await user.send({ embeds: [embedBuilderToDMUser(serverID, serverName)] });
+		const logChannelID = await getLogChannelIDFromDatbase(serverID);
+		if (logChannelID) {
+			const logChannel = interaction.guild?.channels.cache.get(logChannelID) as TextChannel;
+			if (logChannel) {
+				await logChannel.send(`User <@${user.id}> (ID: ${user.id}) has been put into lockdown`);
+			} else {
+				void currentChannel.send("Log channel was not found. The channel was either deleted or the bot has no longer has permissions to it");
+			}
+		} else {
+			void currentChannel.send("Note: The log channel has not been configured");
+		}
     } catch (error) {
         console.error(error);
         await interaction.reply({ content: 'An error occurred while trying to add the lockdown role.', ephemeral: true });
@@ -49,12 +66,6 @@ async function addUserToTheDatabase(user: User, serverID: string, serverName: st
 		console.error(`Error adding to the database for user: ${user.globalName} (ID: ${user.id}), serverID: ${serverID} and serverName: ${serverName}`, error);
         return null; 
 	}
-}
-
-async function getLockdownRoleID(serverID: string) {
-	const theServer = await Servers.findOne({ id: serverID });
-	if (!theServer) return null;
-	return theServer.serverConfig?.lockdownRoleID;
 }
 
 function makeNewUserDocumentWithLockdown(userId: string, username: string, serverID: string, serverName: string) {
@@ -81,4 +92,17 @@ function embedBuilderToDMUser(serverID: string, serverName: string): EmbedBuilde
 	.setTitle(`You have been locked down in ${serverName} (ID: ${serverID})`)
 	.setDescription(`You have been deemed suspicious by the server owners and mods and is currently under lockdown from viewing the server's content. Please contact an admin or mod to get it sorted out`)
 	.setTimestamp();
+}
+
+
+async function getLockdownRoleIDFromDatabase(serverID: string) {
+	const theServer = await Servers.findOne({ id: serverID });
+	if (!theServer) return null;
+	return theServer.serverConfig?.lockdownRoleID;
+}
+
+async function getLogChannelIDFromDatbase(serverID: string) {
+	const theServer = await Servers.findOne({ id: serverID });
+	if (!theServer) return null;
+	return theServer.serverConfig?.lockdownLogChannel;
 }
