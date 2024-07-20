207b395f17ab0d2cc4c9dae4c988b8c4d067e900
WildBabaloo <andphiltre@gmail.com>
2024-07-19 22:43:43 -0400

    The copy paste goes hard but the ticket is done

--- commands/lockdown/unlock_user.ts
+++ commands/lockdown/unlock_user.ts
@@ -1,7 +1,7 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, User, GuildMember } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction, User, GuildMember, EmbedBuilder, TextChannel } from 'discord.js';
 import { Servers } from '../../database/schemas/servers';
 import { Users } from '../../database/schemas/users';
-import { checkIfUserIsUnderLockdownInThatServer, getLockdownRoleIDFromDatabase, getLogChannelIDFromDatbase } from './lockdown_user';
+import { checkIfUserIsUnderLockdownInThatServer, getLockdownRoleIDFromDatabase, getLogChannelIDFromDatabase } from './lockdown_user';
 
 export const data = new SlashCommandBuilder()
 		.setName("unlock_user")
@@ -14,6 +14,13 @@ export async function execute(interaction: ChatInputCommandInteraction) {
     const user = interaction.options.getUser("user");
 	if (!user) {
 		await interaction.reply({content: "Error! The user is invalid", ephemeral: true});
+		return;
+	}
+
+    const currentChannel = interaction.channel;
+	if (!currentChannel) { 
+		console.error("Interaction channel is null."); 
+		await interaction.reply({content: "Error processing this command due to an unknown error (Most likely of discord's part)", ephemeral: true});
 		return;
 	}
 
@@ -38,6 +45,19 @@ export async function execute(interaction: ChatInputCommandInteraction) {
         await member.roles.remove(lockdownRoleID);
         await removeServerFromTheUserSchema(user, serverID, serverName);
         await removeUserFromTheServerSchema(user, serverID);
+        await interaction.reply(`<@${user.id}> has been removed from their lockdown state`);
+        await user.send({ embeds: [embedBuilderToDMUserThatTheyAreNoLongerUnderLockdown(serverID, serverName)] })
+        const logChannelID = await getLogChannelIDFromDatabase(serverID);
+        if (logChannelID) {
+			const logChannel = interaction.guild?.channels.cache.get(logChannelID) as TextChannel;
+			if (logChannel) {
+				await logChannel.send({ embeds: [embedBuilderForLogChannelWhenUserHasBeenLockedDown(user.id, user.username, userAvatar , moderator)] });
+			} else {
+				void currentChannel.send("Log channel was not found. The channel was either deleted or the bot has no longer has permissions to it");
+			}
+		} else {
+			void currentChannel.send("Note: The log channel has not been configured");
+		}
     } catch (error) {
         console.error(error);
         await interaction.reply({ content: 'An error occurred while trying to remove the lockdown role.', ephemeral: true });
@@ -47,6 +67,7 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 async function removeServerFromTheUserSchema(user: User, serverID: string, serverName: string) {
     try {
         await Users.findOneAndUpdate({id: user.id}, {$pull: {"userLogs.activeLockdowns.server.serverID": serverID, "userLogs.activeLockdowns.server.serverName": serverName}});
+        console.log(`Updated user schema for ${user.globalName}. They are no longer marked as lockdowned in ${serverName}`);
     } catch (error) {
         console.error(`Error removing to the database for user: ${user.globalName} (ID: ${user.id}), serverID: ${serverID} and serverName: ${serverName}`, error);
         throw error;
@@ -56,9 +77,32 @@ async function removeServerFromTheUserSchema(user: User, serverID: string, serve
 async function removeUserFromTheServerSchema(user: User, serverID: string) {
     try {
         await Servers.findOneAndUpdate({id: serverID}, {$pull: {"loggedMembers.lockdownedMembers.userID": user.id, "loggedMembers.lockdownedMembers.username": user.globalName}});
+        console.log(`Updated server schema for ${serverID}. User ${user.id} is no longer marked as lockdowned`);
     } catch (error) {
         console.error(`Error removing to the database for user: ${user.globalName} (ID: ${user.id}), serverID: ${serverID}`, error);
         throw error;
     }
+}
+
+function embedBuilderToDMUserThatTheyAreNoLongerUnderLockdown(serverID: string, serverName: string): EmbedBuilder {
+	// TODO customizable title and description (found in ticket 35)
+	return new EmbedBuilder()
+	.setColor(0x30DF30)
+	.setTitle(`You have been locked down in ${serverName} (ID: ${serverID})`)
+	.setDescription(`You have been deemed suspicious by the server owners and mods and is currently under lockdown from viewing the server's content. Please contact an admin or mod to get it sorted out`)
+	.setTimestamp();
+}
+
+function embedBuilderForLogChannelWhenUserHasBeenLockedDown(userID: string, username: string, userAvatar: string | null, moderatorID: string): EmbedBuilder {
+	return new EmbedBuilder()
+	.setColor(0x30DF30)
+	.setThumbnail(userAvatar)
+	.setTitle(`User Unlocked | ${username}`)
+	.addFields(
+		{ name: 'User', value: `<@${userID}>`, inline: true },
+		{ name: 'Moderator', value: `<@${moderatorID}>`, inline: true }
+	)
+	.setFooter({ text: `ID: ${userID}` })
+	.setTimestamp()
 }
 
