c8fa00394c00cc1ee449110051863058cb951fec
WildBabaloo <andphiltre@gmail.com>
2024-07-29 17:56:54 -0400

    Ticket 47 finished

--- commands/lockdown/remove_lockdown_access_roles.ts
+++ commands/lockdown/remove_lockdown_access_roles.ts
@@ -11,5 +11,36 @@ export const data = new SlashCommandBuilder()
     .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator);
 
 export async function execute(interaction: ChatInputCommandInteraction) {
-    await interaction.reply("remove_lockdown_access_role")
+    const member = interaction.member as GuildMember;
+    if (!member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+        await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+        return;
+    }
+
+    const role = interaction.options.getRole("role");
+
+    if (!role || !isRole(role)) {
+        await interaction.reply({content: "This is not a valid role", ephemeral: true});
+        return;
+    }
+
+    const serverID = interaction.guild?.id as string;
+    const serverName = interaction.guild?.name as string;
+    try {
+        await removeAccessRoleFromTheDatabase(role, serverID, serverName);
+        await interaction.reply(`The role <@&${role.id}> has been been revoked access from using the lockdown commands.`);
+    } catch (error) {
+        await interaction.reply("Error with removing the role from our database.");
+        return;
+    }
+}
+
+async function removeAccessRoleFromTheDatabase(role: Role, serverID: string, serverName: string) {
+    try {
+        await Servers.findOneAndUpdate({id: serverID}, {$pull: {"serverConfig.lockdownConfig.lockdownRoleAccess": role.id}});
+        console.log(`Access for the role ${role.id} from the server ${serverName} (ID: ${serverID}) has now been revoked`);
+    } catch (error) {
+        console.error(`Error removing access role in ${serverName} (ID: ${serverID}) for the role under the ID of ${role.id}`, error);
+        throw error;
+    }
 }
