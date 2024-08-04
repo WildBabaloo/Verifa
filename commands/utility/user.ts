b5bb61efb981d1fb87c3d6a7088d1cae2375ae3d
WildBabaloo <andphiltre@gmail.com>
2024-08-03 23:17:20 -0400

    Checkpoint for user info

--- commands/utility/user.ts
+++ commands/utility/user.ts
@@ -1,10 +1,12 @@
-import { SlashCommandBuilder, CommandInteraction, GuildMember } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction, GuildMember, EmbedBuilder, User } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
-		.setName('user')
-		.setDescription('Provides information about the user.');
+		.setName('userinfo')
+		.setDescription('Provides information about the user mentioned.')
+		.addUserOption(option => option.setName("user")
+								.setDescription("Enter the user you would like to lookup"));
 
-export async function execute(interaction: CommandInteraction) {
+export async function execute(interaction: ChatInputCommandInteraction) {
 		if (!interaction.guild) {
 			await interaction.reply("This command can only be used in a server.");
 			return;
@@ -14,8 +16,19 @@ export async function execute(interaction: CommandInteraction) {
 			return;
 		}
 
-		const member = interaction.member;
-		const joinedAt = member.joinedAt ? member.joinedAt.toDateString() : "an unknown date";
+		const user = interaction.options.getUser("user");
+		const member = user instanceof User ? await interaction.guild.members.fetch(user) : interaction.member;
+		await interaction.reply({embeds: [embedBuilderForUserInfo(member)]});
+}
 
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${joinedAt}.`);
+function embedBuilderForUserInfo(member: GuildMember) {
+	return new EmbedBuilder()
+		.setTitle(member.user.username)
+		.setThumbnail(member.user.avatarURL())
+		.setDescription(`<@${member.id}>`)
+		.addFields(
+			{ name: "Joined", value: member.joinedAt?.toDateString() ?? "An Unknown Date", inline: true },
+			{ name: "Registered", value: member.user.createdAt.toDateString() ?? "An Unknown Date", inline: true }
+		)
+		.setTimestamp()
 }
