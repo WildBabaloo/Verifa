6e3ba66373f5a1c4bf53e2c4f84dff4c29ab32d8
WildBabaloo <andphiltre@gmail.com>
2024-08-13 11:50:16 -0400

    Ticket 65 complete!

--- commands/admin/addManagerRoles.ts
+++ commands/admin/addManagerRoles.ts
@@ -25,6 +25,82 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 
     const serverID = interaction.guild?.id as string;
     const serverName = interaction.guild?.name as string;
-    // Check if role is already in manager list
-    // If not then add role to db
+    try {
+        const isManagerRoleInServerSchema = await checkIfManagerRoleIsAlreadyInTheServerSchema(role, serverID);
+        if (isManagerRoleInServerSchema) {
+            await interaction.reply({content: `<@&${role.id}> is already part of the manager roles`, ephemeral: true})
+            return;
+        }
+        await addManagerRoleToTheDatabase(role, serverID, serverName);
+        await interaction.reply(`The role <@&${role.id}> has been been added to list of of managers in the server`);
+    } catch (error) {
+        console.log(`Error with adding ${role.id} onto the database for this server ${serverName} (ID: ${serverID})`, error);
+        await interaction.reply("Error with adding the role onto our database.");
+        return;
+    }
+}
+
+export async function checkIfManagerRoleIsAlreadyInTheServerSchema(role: Role, serverID: string) {
+    const theServer = await Servers.findOne({id: serverID});
+    if (theServer) { 
+        return theServer.serverConfig?.managerRoleIDs.includes(role.id);
+    }
+
+    return false;
+}
+
+async function addManagerRoleToTheDatabase(role: Role, serverID: string, serverName: string) {
+    try {
+        let theServer = await Servers.findOne({id: serverID});
+        if (!theServer) {
+            console.log(`Server ${serverName} (id: ${serverID}) was not found in the database adding it now...`);
+            theServer = makeNewServerDocumentWithManagerRole(role, serverID, serverName);
+            await theServer.save();
+            console.log(`The ${role.name} (id: ${role.id}) role for the server called ${serverName} has been saved to the database as a new role that is now part of the list of managers for that server`);
+        } else {
+            await Servers.findOneAndUpdate({id: serverID}, {$push: {"serverConfig.managerRoleIDs": role.id}});
+            console.log(`Server ${serverName} (id: ${serverID}) was found and its role ${role.name} (id: ${role.id}) has now been added to the list of managers`);
+        }
+    } catch (error) {
+        console.error(`Could not save the server ${serverID} under the name ${serverName} and/or role ${role.id} to the database`, error);
+        throw error;
+    }
+}
+
+function makeNewServerDocumentWithManagerRole(role: Role, serverID: string, serverName: string) {
+    return new Servers({
+        id: serverID,
+        name: serverName,
+        serverConfig: {
+            managerRoleIDs: [role.id],
+            lockdownConfig: {
+                lockdownRoleID: null,
+                lockdownLogChannel: null,
+                lockdownRoleAccess: [],
+                reason: null
+            }
+        },
+        loggedMembers: {
+            globalBannedMembers: {
+                userID: [],
+                username: [],
+                reason: [],
+            },
+            lockdownedMembers: {
+                userID: [],
+                username: [],
+                reason: [],
+            },
+            warnedMembers: {
+                userID: [],
+                username: [],
+                reason: [],
+            },
+            notedMembers: {
+                userID: [],
+                username: [],
+                reason: [],
+            }
+        }
+    });
 }
