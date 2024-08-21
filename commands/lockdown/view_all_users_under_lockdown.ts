d1b04947f955547af39ace082b16ce007ae5917d
WildBabaloo <andphiltre@gmail.com>
2024-08-21 13:57:02 -0400

    View all user under lockdown done but need better way to display relevant information cuz rn it looks like shit

--- commands/lockdown/view_all_users_under_lockdown.ts
+++ commands/lockdown/view_all_users_under_lockdown.ts
@@ -8,15 +8,21 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction: CommandInteraction) {
     const serverID = interaction.guild?.id as string;
     const lockdownedMembers = await grabAllLockdownUsersIDFromTheDatabase(serverID);
-    const lockdownedMembersIDs = lockdownedMembers?.userID as string[];
-    const lockdownedMembersUsernames = lockdownedMembers?.username as string[];
-    const lockdownedMembersReasons = lockdownedMembers?.reason as string[];
-    if (lockdownedMembersIDs.length == 0 && lockdownedMembersUsernames.length == 0 && lockdownedMembersReasons.length == 0) {
+    if (!lockdownedMembers || lockdownedMembers.length == 0) {
         await interaction.reply("There are currently no users under lockdown");
         return;
     }
+    const lockdownedMembersIDs = lockdownedMembers.map(member => member.userID) as string[];
+    const lockdownedMembersUsernames = lockdownedMembers.map(member => member.username) as string[];
+    const lockdownMembersDateTime = lockdownedMembers.map(member => member.dateAndTime) as string[];
+    const lockdownMemebersModerators = lockdownedMembers.map(member => member.moderator) as string[];
+    const lockdownedMembersReasons = lockdownedMembers.map(member => member.reason) as string[];
+    // if (lockdownedMembersIDs.length == 0 && lockdownedMembersUsernames.length == 0 && lockdownedMembersReasons.length == 0) {
+        // await interaction.reply("There are currently no users under lockdown");
+        // return;
+    // }
     // await interaction.reply({ embeds: [embedBuilderForShowingAllLockdownedUsersInAServer(lockdownedMembersIDs, lockdownedMembersReasons)] });
-    await interaction.reply({ embeds: [embedBuilderForShowingAllLockdownedUsersInAServer(lockdownedMembersIDs)] });
+    await interaction.reply({ embeds: [embedBuilderForShowingAllLockdownedUsersInAServer(lockdownedMembersIDs, lockdownedMembersUsernames, lockdownMembersDateTime, lockdownMemebersModerators, lockdownedMembersReasons)] });
 }
 
 async function grabAllLockdownUsersIDFromTheDatabase(serverID: string) {
@@ -24,14 +30,15 @@ async function grabAllLockdownUsersIDFromTheDatabase(serverID: string) {
     return theServer?.loggedMembers?.lockdownedMembers;
 }
 
-// function embedBuilderForShowingAllLockdownedUsersInAServer(lockdownedMembersIDs: string[], lockdownedMembersReasons: string[]) {
-function embedBuilderForShowingAllLockdownedUsersInAServer(lockdownedMembersIDs: string[]) {
+function embedBuilderForShowingAllLockdownedUsersInAServer(lockdownedMembersIDs: string[], lockdownedMembersUsernames: string[], lockdownMembersDateTime: string[], lockdownMemebersModerators: string[], lockdownedMembersReasons: string[]) {
     return new EmbedBuilder()
         .setTitle("List of all the current lockdowned members in the server")
         .addFields(
-            { name: "Users", value: lockdownedMembersIDs.map(id => `<@${id}>`).join("\n") },
-            // The reason part will be filled in for a later ticket
-            // { name: "Reasons", value: lockdownedMembersReasons.join("\n") } 
+            { name: "Username", value: lockdownedMembersUsernames.join("\n"), inline: true },
+            { name: "Users", value: lockdownedMembersIDs.map(id => `<@${id}>`).join("\n"), inline: true },
+            { name: "Date", value: lockdownMembersDateTime.join("\n"), inline: true },
+            { name: "Moderator", value: lockdownMemebersModerators.map(id => `<@${id}>`).join("\n"), inline: true },
+            { name: "Reasons", value: lockdownedMembersReasons.join("\n"), inline: true } 
         )
         .setTimestamp()
 }
